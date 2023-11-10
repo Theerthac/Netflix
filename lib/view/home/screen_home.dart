feab66bbe0171a38addd49fd56632f25eb34fb84
Theerthac <127188105+Theerthac@users.noreply.github.com>
2023-11-10 15:20:22 +0530

    home page

--- lib/view/home/screen_home.dart
+++ lib/view/home/screen_home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/constants/constants.dart';
-import 'package:netflix/view/home/widgets/number_card.dart';
 import 'package:netflix/view/home/widgets/number_tile_card.dart';
-import 'package:netflix/view/widgets/home_card.dart';
 import 'package:netflix/view/widgets/home_title_card.dart';
-import 'package:netflix/view/widgets/main_title.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
